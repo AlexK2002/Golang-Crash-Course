916f63e0b589e9a41bae8ab8a12a1bafb33bb84b
Alex <magsk8@yandex.com>
2020-04-16 23:03:15 +0300

    Update main.go

--- 02-vars/main.go
+++ 02-vars/main.go
@@ -5,24 +5,43 @@ import "fmt"
 func main() {
 	//fmt.Println("Variables: ")
 	//Strings
-	// Intger
+	// Intger int8, in16, int32, int64
 	//Boolean
 	// Float
 
-	//var name string = "Alex"
-	var age = 30
-	const isCool = true
+	// var n bool // default Flase
+	// fmt.Printf("%v, %T\n", n, n)
 
-	// SHort must be declare inside function
+	// var m int // m == 0
+	// fmt.Printf("%v, %T\n", m, m)
+
+	// var num int32 = 42
+	// fmt.Printf("%v, %T\n", num, num)
+
+	a := 10             //1010
+	b := 3              //0011
+	fmt.Println(a & b)  // 0010
+	fmt.Println(a | b)  // 1011
+	fmt.Println(a ^ b)  // 1001
+	fmt.Println(a &^ b) //0100
+
+	c := 8              // 2^3
+	fmt.Println(c << 3) // 2^3 * 2^3 = 2^64
+	fmt.Println(c >> 3) // 2^3 / 2*3 = 2^0
+	//var name string = "John"
+	// var age = 30
+	// const isCool = true
+
+	// Short must be declare inside function
 	//name := "Alex"
 	//size := 1.3
-	var size float32 = 2.3
-	//email := "test@gmail.com"
+	// var size float32 = 2.3
+	// //email := "test@gmail.com"
 
-	name, email := "Alex", "alex@gmail.com"
+	// name, email := "Alex", "alex@gmail.com"
 
-	fmt.Println(name, age, isCool, email)
+	// fmt.Println(name, age, isCool, email)
 
-	fmt.Printf("%T\n", name)
-	fmt.Printf("%T", size)
+	// fmt.Printf("%T\n", name)
+	// fmt.Printf("%T", size)
 }
